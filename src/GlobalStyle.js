6f4ba8550bed1f25603235fc7ff618fd7264007b
Vov4ukz53 <81194780+Vov4ukz53@users.noreply.github.com>
2022-05-16 19:11:59 +0200

    initial commit

---
+++ src/GlobalStyle.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+  *, ::after, ::before {
+    box-sizing: inherit;
+  }
+  body {
+    font-family: 'Poppins', sans-serif;
+    font-size: 16px;
+  }
+`;
