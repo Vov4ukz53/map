9eacf9d7c52d35ddfc75b4777c079c0c813630ef
Vov4ukz53 <81194780+Vov4ukz53@users.noreply.github.com>
2022-05-18 22:48:06 +0200

    created markers icons

---
+++ src/features/mapPage/markers/index.js
@@ -0,0 +1,11 @@
+import L from "leaflet";
+
+export const markerIconStart = new L.Icon({
+  iconUrl: require("./images/iconStart.png"),
+  iconSize: [35, 45],
+});
+
+export const markerIconEnd = new L.Icon({
+  iconUrl: require("./images/iconEnd.png"),
+  iconSize: [35, 45],
+});
