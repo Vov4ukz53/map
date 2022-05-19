fc7338de3398cfcd769b7f0f671870d9792d7cfe
Vov4ukz53 <81194780+Vov4ukz53@users.noreply.github.com>
2022-05-19 16:57:26 +0200

    creating apiData and routingMachine

---
+++ src/apiData.js
@@ -0,0 +1,3 @@
+export const url = "https://api.tomtom.com/search/2/geocode/";
+export const API_KEY = "PxU9DLSsk4z2MO6cLU7uj4lm8mhY9H9I";
+
---
+++ src/features/mapPage/RoutingMachine.js
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { selectAddressEnd, selectAddressStart } from "../mapSlice";
+import { useMap } from "react-leaflet";
+import L from "leaflet";
+import "leaflet-routing-machine/dist/leaflet-routing-machine.css";
+import "leaflet-routing-machine";
+
+L.Marker.prototype.options.icon = L.icon({
+  iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png"
+});
+
+export const RoutingMachine = () => {
+  const { position } = useSelector(selectAddressStart);
+  const addressEnd = useSelector(selectAddressEnd);
+  const map = useMap();
+
+  useEffect(() => {
+    if (!map) return;
+
+    position && addressEnd && L.Routing.control({
+      waypoints: [
+        L.latLng(position.lat, position.lon),
+        L.latLng(addressEnd[0].position.lat, addressEnd[0].position.lon),
+      ],
+      show: false,
+    }).addTo(map);
+  }, [map]);
+
+  return null;
+};
