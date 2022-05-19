098f3f3988e2e1e9a894b2d39ba582a91681331c
Vov4ukz53 <81194780+Vov4ukz53@users.noreply.github.com>
2022-05-19 16:15:13 +0200

    added useReplaceQueryParameter

---
+++ src/features/queryParameters.js
@@ -0,0 +1,28 @@
+import { useLocation, useNavigate } from "react-router";
+
+export const useQueryParameter = (key) => {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+
+  return searchParams.get(key);
+};
+
+export const useReplaceQueryParameter = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const searchParams = new URLSearchParams(location.search);
+
+  return ({ key, value }) => {
+    if (value === "") {
+      searchParams.delete(key);
+    } else {
+      searchParams.set(key, value);
+    }
+
+    navigate({
+      pathname: location.pathname,
+      search: searchParams.toString()
+    });
+  }
+};
+
