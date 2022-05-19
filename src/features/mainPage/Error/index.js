2c986643c30fbad1484387c7e9446e78475cee1e
Vov4ukz53 <81194780+Vov4ukz53@users.noreply.github.com>
2022-05-19 17:52:07 +0200

    created error info and added to main page

---
+++ src/features/mainPage/Error/index.js
@@ -0,0 +1,9 @@
+import { Wrapper, Paragraph } from "./styled"
+
+export const Error = () => (
+  <Wrapper>
+    <Paragraph>
+      Please check your internet connection or try again later!
+    </Paragraph>
+  </Wrapper>
+);
--- src/features/mainPage/MainPage.js
+++ src/features/mainPage/MainPage.js
@@ -1,9 +1,10 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useQueryParameter } from "../queryParameters";
-import { fetchMap } from "../mapSlice";
+import { fetchMap, selectError } from "../mapSlice";
 import { Container } from "../../common/Container";
 import { Form } from "./Form";
+import { Error } from "./Error";
 import {
   Wrapper,
   Title,
@@ -14,6 +15,7 @@ export const MainPage = () => {
   const dispatch = useDispatch();
   const query = useQueryParameter("search");
   const query1 = useQueryParameter("search1");
+  const error = useSelector(selectError);
 
   useEffect(() => {
     query !== null && query1 !== null &&
@@ -25,10 +27,11 @@ export const MainPage = () => {
       <Container>
         <Title>Main Title</Title>
         <Form />
-        {query && query1 &&
+        {error ? <Error />
+          : query && query1 && !error &&
           <Link to="/map">Show result</Link>
         }
       </Container>
-    </Wrapper>
+    </Wrapper >
   )
 };
