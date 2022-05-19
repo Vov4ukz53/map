5f1aa37342aeadeb6902eccf5f9953139bd562fc
Vov4ukz53 <81194780+Vov4ukz53@users.noreply.github.com>
2022-05-19 16:59:26 +0200

    added provider and store

--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
+import store from './store';
 import { theme } from './theme';
 import { GlobalStyle } from './GlobalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
 
