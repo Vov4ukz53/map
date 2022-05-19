b2fb070ee07883cbc710e59199f09377e736500c
Vov4ukz53 <81194780+Vov4ukz53@users.noreply.github.com>
2022-05-19 17:02:54 +0200

    added routing machine

--- src/features/mapPage/mapPage.js
+++ src/features/mapPage/mapPage.js
@@ -1,9 +1,14 @@
+import { useSelector } from "react-redux";
 import { Container } from "../../common/Container";
 import { TileLayer } from "react-leaflet";
 import { MyMap } from "./styled";
+import { selectMap, selectMap2 } from "../mapSlice";
 import "leaflet/dist/leaflet.css";
+import { RoutingMachine } from "./RoutingMachine";
 
 export const MapPage = () => {
+  // const { position } = useSelector(selectMap);
+  // const map2 = useSelector(selectMap2);
   const coord = [51.12339388765598, 17.004908669261074];
 
   return (
@@ -13,6 +18,7 @@ export const MapPage = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        <RoutingMachine />
       </MyMap>
     </Container>
   )
