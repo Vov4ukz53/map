6e9456b817d70b714a294d152265fa166c2569d3
Vov4ukz53 <81194780+Vov4ukz53@users.noreply.github.com>
2022-05-19 17:01:41 +0200

    added replace query parameter to inputs

--- src/features/mainPage/Form/index.js
+++ src/features/mainPage/Form/index.js
@@ -1,54 +1,42 @@
-import { useLocation, useNavigate } from "react-router";
-import { StyledForm, Input, Button } from "./styled";
+import debounce from "lodash.debounce";
+import { useReplaceQueryParameter } from "../../queryParameters";
+import { StyledForm, Input } from "./styled";
 
 export const Form = () => {
-  const location = useLocation();
-  const navigate = useNavigate();
+  const replaceQueryParameter = useReplaceQueryParameter();
 
-  const onInputChange = ({ target }) => {
-    const searchParams = new URLSearchParams(location.search);
+  const onInputChange = debounce(({ target }) => {
+    replaceQueryParameter({
+      value: target.value.trim(),
+      key: "search",
+    })
+  }, 1000);
 
-    if (target.value.trim() === "") {
-      searchParams.delete("search");
-    } else {
-      searchParams.set("search", target.value);
-    }
-
-    navigate({
-      pathname: location.pathname,
-      search: searchParams.toString()
-    });
-  };
-
-  const onInputNextChange = ({ target }) => {
-    const searchParams = new URLSearchParams(location.search);
-
-    if (target.value.trim() === "") {
-      searchParams.delete("search1");
-    } else {
-      searchParams.set("search1", target.value);
-    }
-
-    navigate({
-      pathname: location.pathname,
-      search: searchParams.toString()
-    });
-  };
+  const onInputNextChange = debounce(({ target }) => {
+    replaceQueryParameter({
+      value: target.value.trim(),
+      key: "search1",
+    })
+  }, 1000);
 
   return (
-    <StyledForm>
-      <Input
-        onChange={onInputChange}
-        type="text"
-        name="start"
-        placeholder="Origin"
-      />
-      <Input
-        onChange={onInputNextChange}
-        type="text"
-        name="end"
-        placeholder="Destination" />
-      <Button>Calculate Route</Button>
-    </StyledForm>
+    <>
+      <StyledForm>
+        <Input
+          onChange={onInputChange}
+          type="text"
+          name="start"
+          placeholder="Origin"
+          tabIndex={1}
+        />
+        <Input
+          onChange={onInputNextChange}
+          type="text"
+          name="end"
+          placeholder="Destination"
+          tabIndex={2}
+        />
+      </StyledForm>
+    </>
   )
-}
+};
